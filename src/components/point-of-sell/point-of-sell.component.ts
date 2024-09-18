91357aa2e417fede26597c382556cfae3925e940
405312-Benegas-Joaquin <405312@tecnicatura.frc.utn.edu.ar>
2024-09-18 20:36:58 -0300

    initial scaffolding

---
+++ src/components/point-of-sell/point-of-sell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-point-of-sell',
+  standalone: true,
+  imports: [],
+  templateUrl: './point-of-sell.component.html',
+  styleUrl: './point-of-sell.component.css'
+})
+export class PointOfSellComponent {
+
+}
